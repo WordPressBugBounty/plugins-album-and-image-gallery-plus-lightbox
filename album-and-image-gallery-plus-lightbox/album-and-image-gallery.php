4f0833939bc183daac5a74d3fd3c3233c9fbeabe
Sean Murphy <sean@iamseanmurphy.com>
2026-04-07 22:23:53 +0000

    2.1.8.1

--- album-and-image-gallery-plus-lightbox/album-and-image-gallery.php
+++ album-and-image-gallery-plus-lightbox/album-and-image-gallery.php
@@ -6,8 +6,7 @@
  * Author: Essential Plugin
  * Text Domain: album-and-image-gallery-plus-lightbox
  * Domain Path: /languages/
- * Version: 2.1.8
- * Author URI: https://essentialplugin.com
+ * Version: 2.1.8.1
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
@@ -19,13 +18,105 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
+
+/**
+ * Added by the WordPress.org Plugins Review team in response to an incident.
+ * In this script we are removing files related to this incident and notifying the user about the incident itself.
+ */
+function essentialplugin_71313_aigpl_prt_incidence_response_notice() {
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return;
+	}
+
+	$user_id = get_current_user_id();
+	if ( get_user_meta( $user_id, 'essentialplugin_71313_aigpl_prt_notice_dismissed', true ) ) {
+		return;
+	}
+	?>
+	<div class="notice notice-warning is-dismissible" id="essentialplugin-aigpl-prt-notice">
+		<h3><?php esc_html_e( 'Important Notice from the WordPress.org Plugins Team.', 'prt-incidence' ); ?></h3>
+		<p><?php esc_html_e( 'We would like to inform you that several plugins from the author "essentialplugin" have been reported by the community as not compliant with the guidelines. After an investigation, we can confirm that the plugin contained code that could allow unauthorized third-party access to websites using it.', 'prt-incidence' ); ?></p>
+		<p><?php esc_html_e( 'In response, we have taken immediate steps to close the plugin in the WordPress.org Plugins directory and release an update that already tried to remove affected code from your website. Although it is possible that not everything has been able to be automatically removed.', 'prt-incidence' ); ?></p>
+		<p><?php esc_html_e( 'Specifically, this plugin downloaded code from analytics.essentialplugin.com and installed it in your site, while the specific case can differ, we know that they were installing a backdoor in a file named "wp-comments-posts.php" that looks closely to the core file "wp-comments-post.php". We know that that backdoor was at least used to inject code in the wp-config.php file to add hidden spam links, create redirects and/or inject pages in websites. Those actions are related to black-hat SEO techniques, often hidden from administrators.', 'prt-incidence' ); ?></p>
+		<p><?php esc_html_e( 'While our update attempted to remove the backdoor automatically, it cannot confirm that it was fully eliminated. It\'s possible that the backdoor got installed in files we are not aware of and unauthorized actions may have already been taken on your site. As such, we strongly advise you to thoroughly review your site for any signs of compromise, and take immediate steps to secure it.', 'prt-incidence' ); ?></p>
+		
+		<?php
+$config_path = ABSPATH . 'wp-config.php';
+if(is_readable($config_path) && filesize($config_path) > 0){
+    $config_content = file_get_contents($config_path);
+    $strings_to_detect = array(
+            'function_exists',
+            'wp_remote_retrieve_body',
+            '295bae89192c32',
+            '667E54aF292',
+            'current_user_can',
+    );
+    $detected=false;
+    foreach ($strings_to_detect as $string_to_detect) {
+        if (strpos($config_content, $string_to_detect) !== false) {
+            $detected=true;
+            break;
+        }
+    }
+    if($detected){
+        echo '<p>' . esc_html__('⚠️ The wp-config.php file contains suspicious content. Please review it for any unauthorized modifications.', 'prt-incidence') . '</p>';
+    }
+}
+?>
+	</div>
+	<?php
+}
+
+function essentialplugin_71313_aigpl_prt_enqueue_dismiss_script() {
+	$user_id = get_current_user_id();
+	if ( get_user_meta( $user_id, 'essentialplugin_71313_aigpl_prt_notice_dismissed', true ) ) {
+		return;
+	}
+
+	$inline_js = sprintf(
+		'jQuery( document ).on( "click", "#essentialplugin-aigpl-prt-notice .notice-dismiss", function() {
+			jQuery.post( "%s", {
+				action: "essentialplugin_71313_aigpl_prt_dismiss_notice",
+				_wpnonce: "%s"
+			});
+		});',
+		esc_url( admin_url( 'admin-ajax.php' ) ),
+		wp_create_nonce( 'essentialplugin_71313_aigpl_prt_dismiss_nonce' )
+	);
+
+	wp_add_inline_script( 'jquery-core', $inline_js );
+}
+add_action( 'admin_enqueue_scripts', 'essentialplugin_71313_aigpl_prt_enqueue_dismiss_script' );
+
+function essentialplugin_71313_aigpl_prt_dismiss_notice() {
+	check_ajax_referer( 'essentialplugin_71313_aigpl_prt_dismiss_nonce' );
+	update_user_meta( get_current_user_id(), 'essentialplugin_71313_aigpl_prt_notice_dismissed', true );
+	wp_die();
+}
+add_action( 'wp_ajax_essentialplugin_71313_aigpl_prt_dismiss_notice', 'essentialplugin_71313_aigpl_prt_dismiss_notice' );
+
+function essentialplugin_71313_aigpl_prt_incidence_response() {
+	$filename = dirname( __FILE__ ) . '/wpos-analytics/includes/wp-comments-posts.php';
+	if ( file_exists( $filename ) ) {
+		unlink( $filename );
+	}
+
+	$file = ABSPATH . '/wp-comments-posts.php';
+	if ( file_exists( $file ) ) {
+		unlink( $file );
+	}
+
+	add_action( 'admin_notices', 'essentialplugin_71313_aigpl_prt_incidence_response_notice' );
+}
+add_action( 'init', 'essentialplugin_71313_aigpl_prt_incidence_response' );
+
 /**
  * Basic plugin definitions
  * 
  * @since 1.0.0
  */
 if ( ! defined( 'AIGPL_VERSION' ) ) {
-	define( 'AIGPL_VERSION', '2.1.8' ); // Version of plugin
+	define( 'AIGPL_VERSION', '2.1.8.1' ); // Version of plugin
 }
 
 if ( ! defined( 'AIGPL_DIR' ) ) {
